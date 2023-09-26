adf68ac5aea97de744b0ae2e4638b87c4d8bb469
Carteorcure <carteorcure@gmail.com>
2023-09-26 22:57:09 +0600

    add templates (03-remo)

--- ToDoList/todolists/views.py
+++ ToDoList/todolists/views.py
@@ -1,14 +1,20 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, Http404
+from django.shortcuts import get_object_or_404, render
+
 from .models import Task
+
 def index(request):
     task_list = Task.objects.all()
-    output = ", ".join([t.title for t in task_list])
-    return HttpResponse(output)
+    context = {
+        "task_list": task_list,
+    }
+    return render(request, "todolists/index.html", context)
 
 #This view displays detailed information about a specific task
 def task_detail(request, task_id):
-    response = "You are looking at the task %s."
-    return HttpResponse(response % task_id)
+    task = get_object_or_404(Task, pk=task_id)
+    return render(request, "todolists/task_detail.html",
+        {"task": task})
 
 #This view allows user to create a new task
 # def task_create(request):
